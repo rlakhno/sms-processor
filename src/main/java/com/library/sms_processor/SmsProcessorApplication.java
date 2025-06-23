dc76704383b913bbee749c4c0bd98062f7323a67
Rouslan Lakhno <Rouslan.Lakhno@barrielibrary.ca>
2025-06-23 16:24:35 -0400

    Initial commit

---
+++ src/main/java/com/library/sms_processor/SmsProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.library.sms_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library/sms_processor/SmsProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.sms_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
